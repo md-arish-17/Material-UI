410578302e5539c74c097ddc93d676a5a1de32fb
md-arish-17 <it21.mdarish@svceindore.ac.in>
2025-12-30 12:07:31 +0530

    AppBar

--- src/components/MuiNavigation.jsx
+++ src/components/MuiNavigation.jsx
@@ -1,13 +1,20 @@
-import { AppBar, Button, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 const MuiNavigation = () => {
+
+    const [value, setValue] = useState(0)
+
   return (
     <div>
         <AppBar>
             <Toolbar>
                 <Typography variant='h3'>LOGO</Typography>
-                <Button variant='contained' color='success' sx={{marginLeft:"auto"}}>Sign In</Button>
+                <Tabs sx={{marginLeft:"auto"}} textColor='white' value={value} onChange={(e,val)=>{setValue(val)}} indicatorColor='secondary'>
+                    <Tab label="Home"/>
+                    <Tab label="About"/>
+                    <Tab label="Contact"/>
+                </Tabs>
             </Toolbar>
         </AppBar>
     </div>
